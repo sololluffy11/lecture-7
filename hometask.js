423efdbe72b3e29c9bc568b2b06030462b04d3d5
sololuffy1 <sololevelling12@gmail.com>
2023-12-04 21:19:39 +0300

    first commit

---
+++ hometask.js
@@ -0,0 +1,7 @@
+// function task1(a){
+// let date = new Date()
+// let year = new Date(date.setFullYear(a))
+// return Math.ceil(year.getFullYear(a)/100)
+// }
+// console.log(task1(1901));
+
---
+++ main.js
@@ -0,0 +1,48 @@
+// let date = new Date()
+// console.log(date);
+
+// New date (milliseconds)
+// let date = new Date(1234567890)
+// console.log(date);
+
+// New date (Date string)
+// let date = new Date("2023-10-04")
+// console.log(date);
+
+// New date(year,month,day...)
+// let date = new Date(2023,4,12)
+// console.log(date);
+
+// New date()- now()
+// let date = new Date()
+// console.log(date.getTime());
+
+// New date()- getFullYear()
+// let date = new Date()
+// console.log(date.getFullYear());
+
+// New date()- getMonth()
+// let date = new Date()
+// console.log(date.getMonth());
+
+// New date()- getDate()
+// let date = new Date()
+// console.log(date.getDate());
+
+// New date()- getDay()
+// let date = new Date()
+// console.log(date.getDay());
+
+// New date()- getHours()
+// let date = new Date()
+// console.log(date.getHours());
+
+// New date()- getMinutes()
+// let date = new Date()
+// console.log(date.getMinutes());
+
+// New date()- to connect
+// let date = new Date()
+// console.log(date.getMinutes()+" "+date.getSeconds());
+
+
